8533764d57ec17e9168b12a4184c287e742b706f
YAO1267 <S10268880@connect.np.edu.sg>
2025-01-23 11:31:24 +0800

    Initial commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268880K_PRG2Assignment
+{
+    class Flight
+    {
+		public string flightNumber { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public DateTime expectedTime { get; set; }
+        public string status { get; set; }
+
+        public Flight(string fNo, string o, string d, DateTime e, string s)
+        {
+            flightNumber = fNo;
+            origin = o;
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//==========================================================
+// Student Number : S10268880K
+// Student Name : Yao Yao
+// Partner Name : Atifah 
+//==========================================================
+
