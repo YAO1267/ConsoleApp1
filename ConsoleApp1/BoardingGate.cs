ae943f5cd726df412564f8d6f38b74fd7cbb6f72
YAO1267 <S10268880@connect.np.edu.sg>
2025-01-24 15:05:26 +0800

    touch on terminal and boardinggate classes

---
+++ ConsoleApp1/BoardingGate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268880K_PRG2Assignment
+{
+    class BoardingGate
+    {
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWTT { get; set; }
+        public Flight Flight { get; set; }
+
+        public BoardingGate()
+        {
+            
+        }
+
+        public BoardingGate(string gName, bool sCFFT, bool sDDJB, bool sLWTT, Flight f)
+        {
+            GateName = gName;
+            SupportsCFFT = sCFFT;
+            SupportsDDJB = sDDJB;
+            SupportsLWTT = sLWTT;
+            Flight = f;
+        }
+
+        //public double CalculateFees()
+        //{
+
+        //}
+        public override string ToString()
+        {
+            return "Gate Name:" + GateName + "\tSupports CFFT" + SupportsCFFT + "\tSupports DDJB:" + SupportsDDJB + "\tSupports LWTT" + SupportsLWTT + "\tFlight" + Flight;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Terminal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268880K_PRG2Assignment
+{
+    class Terminal
+    {
+        public string TerminalName { get; set; }
+        public Dictionary<string,Airline> Airlines { get; set; }
+        public Dictionary<string,Flight> Flights { get; set; }
+        public Dictionary<string,BoardingGate> BoardingGates { get; set; }
+        public Dictionary<string,double> GateFees { get; set; }
+
+        public Terminal()
+        {
+            
+        }
+        public Terminal(string tName, Dictionary<string, Airline> airL, Dictionary<string, Flight> f, Dictionary<string, BoardingGate> bGate, Dictionary<string, double> gFees)
+        {
+            TerminalName = tName;
+            Airlines = airL;
+            Flights = f;
+            BoardingGates = bGate;
+            GateFees = gFees;
+        }
+
+        public bool AddAirline(Airline airline)
+        {
+            Console.Write("Enter Flight Number:");
+            string newAirline = Console.ReadLine();
+            if (airline.Name != newAirline)
+            {
+                return true;
+            }
+            return false;
+        }       
+        //public bool AddBoardingGate(BoardingGate boardingGate)
+        //{
+        //    if()
+        //}
+    }   
+}
